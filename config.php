5645a43fd6fe627189a2cb4cc6f9e0191491004f
pgiacometto <pedro.giacometto@gmail.com>
2016-08-09 13:16:28 -0430

    OP Install + vqmod

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://lakari/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://lakari/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/localhost/www/lakari/catalog/');
+define('DIR_SYSTEM', 'C:/localhost/www/lakari/system/');
+define('DIR_IMAGE', 'C:/localhost/www/lakari/image/');
+define('DIR_LANGUAGE', 'C:/localhost/www/lakari/catalog/language/');
+define('DIR_TEMPLATE', 'C:/localhost/www/lakari/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/localhost/www/lakari/system/config/');
+define('DIR_CACHE', 'C:/localhost/www/lakari/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/localhost/www/lakari/system/storage/download/');
+define('DIR_LOGS', 'C:/localhost/www/lakari/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/localhost/www/lakari/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/localhost/www/lakari/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mpdo');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'lakari_op');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
