a045bc95d400a605e14d58f52665e7f89bc6ef88
pgiacometto <pedro.giacometto@gmail.com>
2016-08-18 10:37:51 -0430

    Install Template

---
+++ vqmod/vqcache/vq2-catalog_language_es-es_common_cart.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_items']    = '%s art&iacute;culo(s) - %s';
+$_['text_empty']    = 'Tu carro esta vac&iacute;o!';
+$_['text_cart']     = 'Ver Carrito';
+$_['text_checkout'] = 'Caja';
+
+	$_['text_items'] = '<i class="fa fa-shopping-cart"></i><span class="count">%s</span> <span class="total contrast_font mobile_hide">%s</span>';
+			
+$_['text_recurring']  = 'Perfil de pago';
